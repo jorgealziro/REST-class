bf032d6fe07deae6861bafa140118c273ce1bed4
jorgealziro <jorge.ardcs@gmail.com>
2020-03-04 20:41:16 -0300

    Added object constructor examples to objects.js

--- basics/objects.js
+++ basics/objects.js
@@ -17,3 +17,32 @@ console.log(`My byke is a ${bike['make']} ${bike.model} which is ${bike.body} in
 // Calling object methods
 console.log(`${bike.bikeName()} rocks!`);
 
+// Object constructor function example
+function Backpack(make, material, year){
+    this.make = make;
+    this.material = material;
+    this.year = year;
+    this.getFullDescription = function() {
+        return `${this.year} ${this.material} ${this.make} BP`;
+    };
+};
+
+// Creating new instances
+let sportSack = new Backpack('Nike', 'plastic', 2015);
+
+console.log(sportSack.getFullDescription());
+
+// Alternative ES6 constructor
+function Backpack(make, material, year){
+    return{
+        make,
+        material,
+        year,
+        getFullDescription() {
+            return `${this.year} ${this.material} ${this.make} BP`;
+        }
+    };
+};
+
+let myBP = new Backpack('Swiss', 'polyester', 2011);
+console.log(myBP.getFullDescription())
