e7eabf1a84edd0c25bd5b0392a10fdcbde12845d
jorgealziro <jorge.ardcs@gmail.com>
2020-03-05 21:35:52 -0300

    added require and import examples to basics dir

---
+++ basics/export.js
@@ -0,0 +1,16 @@
+export let aVar = 243;
+
+export let func1 = () => {
+    console.log('This is func1');
+};
+
+export let func2 = () => {
+    console.log('This is func2');
+};
+
+// Old format
+// module.exports = {
+//     bVar: aVar,
+//     func1: func1,
+//     func2: func2
+// }
---
+++ basics/requireImport.js
@@ -0,0 +1,6 @@
+// let myFuncs = require('./export');
+import {aVar, func1, func2} from './export';
+
+console.log(`My variable is a ${aVar}`);
+func1();
+func2();
